541312ca0cc2daa5f6a66ae52d0775ab804a97ea
Nickiel12 <nickiel@nickiel.net>
2024-07-12 01:26:10 +0000

    basic `nix shell .#`-able file

--- flake.nix
+++ flake.nix
@@ -3,26 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let 
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+  outputs = { self, nixpkgs, poetry2nix }:
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
 
-        let mbslave = mkPoetryApplication {
-          python = pkgs.python311;
-          projectDir = pkgs.fetchFromGitHub {
-            owner = "acoustid";
-            repo = "mbslave";
-            rev = "v29.0.0";
-            sha256 = "";
-          };
+      mbslave = mkPoetryApplication {
+        python = pkgs.python311;
+        preferWheels = true;
+        projectDir = pkgs.fetchFromGitHub {
+          owner = "acoustid";
+          repo = "mbslave";
+          rev = "v29.0.0";
+          sha256 = "sha256-MdtGLAUA5J0i4pNzbU4pTOeZN78lBRKtb6pHRdY7j6k=";
         };
-      in 
-      {
-        package.mbslave = mbslave;
-      });
+      };
+    in 
+    {
+   
+      packages.${system} = {
+        default = self.packages.${system}.mbslave;
+        mbslave = mbslave;
+      };
+    };
 }
